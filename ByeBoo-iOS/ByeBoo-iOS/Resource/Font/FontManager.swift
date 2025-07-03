bd1619f236f150ee29af35e00eba34e7d860cd45
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-03 18:42:25 +0900

    refactor: #9 camel case로 수정

--- ByeBoo-iOS/ByeBoo-iOS/Resource/Font/FontManager.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Resource/Font/FontManager.swift
@@ -15,53 +15,53 @@ public struct FontProperty {
 }
 
 public enum FontManager {
-    case head1_sb_24
-    case head2_m_22
+    case head1Sb24
+    case head2M22
     
-    case sub1_sb_20
-    case sub2_sb_18
-    case sub3_m_18
-    case sub4_r_18
+    case sub1Sb20
+    case sub2Sb18
+    case sub3M18
+    case sub4R18
     
-    case body1_sb_16
-    case body2_m_16
-    case body3_r_16
-    case body4_sb_14
-    case body5_r_14
+    case body1Sb16
+    case body2M16
+    case body3R16
+    case body4Sb14
+    case body5R14
     
-    case cap1_m_12
-    case cap2_r_12
+    case cap1M12
+    case cap2R12
     
     public var fontProperty: FontProperty {
         switch self {
-        case .head1_sb_24:
+        case .head1Sb24:
             return FontProperty(font: .semibold, size: 24, lineHeight: 130, kern: -1)
-        case .head2_m_22:
+        case .head2M22:
             return FontProperty(font: .medium, size: 22, lineHeight: 120, kern: -1)
             
-        case .sub1_sb_20:
+        case .sub1Sb20:
             return FontProperty(font: .semibold, size: 20, lineHeight: 120, kern: -1)
-        case .sub2_sb_18:
+        case .sub2Sb18:
             return FontProperty(font: .semibold, size: 18, lineHeight: 120, kern: -1)
-        case .sub3_m_18:
+        case .sub3M18:
             return FontProperty(font: .medium, size: 18, lineHeight: 120, kern: -1)
-        case .sub4_r_18:
+        case .sub4R18:
             return FontProperty(font: .regular, size: 18, lineHeight: 120, kern: -1)
             
-        case .body1_sb_16:
+        case .body1Sb16:
             return FontProperty(font: .semibold, size: 16, lineHeight: 130, kern: -1)
-        case .body2_m_16:
+        case .body2M16:
             return FontProperty(font: .medium, size: 16, lineHeight: 130, kern: -1)
-        case .body3_r_16:
+        case .body3R16:
             return FontProperty(font: .regular, size: 16, lineHeight: 130, kern: -1)
-        case .body4_sb_14:
+        case .body4Sb14:
             return FontProperty(font: .semibold, size: 14, lineHeight: 130, kern: -1)
-        case .body5_r_14:
+        case .body5R14:
             return FontProperty(font: .regular, size: 14, lineHeight: 130, kern: -1)
             
-        case .cap1_m_12:
+        case .cap1M12:
             return FontProperty(font: .medium, size: 12, lineHeight: 130, kern: -1)
-        case .cap2_r_12:
+        case .cap2R12:
             return FontProperty(font: .regular, size: 12, lineHeight: 130, kern: -1)
             
         }
