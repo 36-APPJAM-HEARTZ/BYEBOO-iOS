83de0e2471af487ec794a42dac61fca2f1a407e8
JURI <80569323+juri123123@users.noreply.github.com>
2025-07-06 12:43:36 +0900

    feat: #16 base response 정의

---
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Model/BaseResponse.swift
@@ -0,0 +1,14 @@
+//
+//  BaseResponse.swift
+//  ByeBoo-iOS
+//
+//  Created by 최주리 on 7/6/25.
+//
+
+import Foundation
+
+struct BaseResponse<T: Decodable>: Decodable {
+    let success: Bool
+    let message: String
+    let data: T?
+}
