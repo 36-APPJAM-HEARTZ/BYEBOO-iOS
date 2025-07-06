477783d94d3d0d7bc9441523f744ce60c21ae1b5
JURI <80569323+juri123123@users.noreply.github.com>
2025-07-06 16:34:54 +0900

    chore: #16 필요없는 코드 삭제 & usecase concurrency 연결

--- ByeBoo-iOS/ByeBoo-iOS/Data/Network/Service/NetworkService.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Network/Service/NetworkService.swift
@@ -28,41 +28,36 @@ struct DefaultNetworkService: NetworkService {
         ByeBooLogger.network("Headers: \(endPoint.headers.value)")
         ByeBooLogger.network("Parameters: \(String(describing: endPoint.bodyParameters))")
         
-        do {
-            return try await withCheckedThrowingContinuation { continuation in
-                AF.request(
-                    endPoint.requestURL,
-                    method: endPoint.method,
-                    parameters: endPoint.bodyParameters,
-                    encoding: endPoint.parameterEncoding,
-                    headers: endPoint.headers.value
-                )
-                .validate()
-                .responseDecodable(of: decodingType) { response in
-                    switch response.result {
-                    case .success(let data):
-                        ByeBooLogger.data("Decoded Data: \(data)")
-                        continuation.resume(returning: data)
-                    case .failure:
-                        if let data = response.data,
-                           let statusCode = response.response?.statusCode,
-                           let errorResponse = try? JSONDecoder().decode(EmptyResponse.self, from: data) {
-                            let error = ByeBooError.networkError(
-                                code: statusCode,
-                                message: errorResponse.message
-                            )
-                            ByeBooLogger.error(error)
-                            continuation.resume(throwing: error)
-                        }
-                        
-                        ByeBooLogger.error(ByeBooError.decodingError)
-                        continuation.resume(throwing: ByeBooError.decodingError)
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(
+                endPoint.requestURL,
+                method: endPoint.method,
+                parameters: endPoint.bodyParameters,
+                encoding: endPoint.parameterEncoding,
+                headers: endPoint.headers.value
+            )
+            .validate()
+            .responseDecodable(of: decodingType) { response in
+                switch response.result {
+                case .success(let data):
+                    ByeBooLogger.data("Decoded Data: \(data)")
+                    continuation.resume(returning: data)
+                case .failure:
+                    if let data = response.data,
+                       let statusCode = response.response?.statusCode,
+                       let errorResponse = try? JSONDecoder().decode(EmptyResponse.self, from: data) {
+                        let error = ByeBooError.networkError(
+                            code: statusCode,
+                            message: errorResponse.message
+                        )
+                        ByeBooLogger.error(error)
+                        continuation.resume(throwing: error)
                     }
+                    
+                    ByeBooLogger.error(ByeBooError.decodingError)
+                    continuation.resume(throwing: ByeBooError.decodingError)
                 }
             }
-        } catch {
-            ByeBooLogger.error(ByeBooError.networkRequestFailed)
-            throw ByeBooError.networkRequestFailed
         }
     }
     
