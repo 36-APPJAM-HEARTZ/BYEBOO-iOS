122f6e74dcd2c045bc681d728fb66c7661a8f24b
JURI <80569323+juri123123@users.noreply.github.com>
2025-07-09 02:05:39 +0900

    style: #65 최초 여정 배정 결과 UI 구현

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Home/View/JourneyResultView.swift
@@ -0,0 +1,119 @@
+//
+//  JourneyResultView.swift
+//  ByeBoo-iOS
+//
+//  Created by 최주리 on 7/9/25.
+//
+
+import UIKit
+
+enum JourneyType {
+    case face
+    case process
+    
+    var image: UIImage {
+        switch self {
+        case .face:
+                .faceEmotion
+        case .process:
+                .processEmotion
+        }
+    }
+}
+
+final class JourneyResultView: BaseView {
+
+    private let backgroundImageView = UIImageView()
+    private let backgroundView = UIView()
+    private let titleLabel = UILabel()
+    private let imageView = UIImageView()
+    private let descriptionLabel = UILabel()
+    let confirmLabel = UILabel()
+    
+    private let name: String
+    private let journeyType: JourneyType
+    private let journeyDescription: String
+    
+    init(
+        name: String,
+        journeyType: JourneyType,
+        journeyDescription: String
+    ) {
+        self.name = name
+        self.journeyType = journeyType
+        self.journeyDescription = journeyDescription
+        
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setStyle() {
+        backgroundImageView.do {
+            $0.image = .bgLight
+        }
+        backgroundView.do {
+            $0.backgroundColor = .black50
+        }
+        titleLabel.do {
+            $0.text = "지금 \(name) 님에게 필요한 건"
+            $0.font = FontManager.body1Sb16.font
+            $0.textColor = .white
+        }
+        imageView.do {
+            $0.image = journeyType.image
+        }
+        descriptionLabel.do {
+            $0.text = journeyDescription
+            $0.numberOfLines = 0
+            $0.font = FontManager.body5R14.font
+            $0.textColor = .secondary100
+            $0.textAlignment = .center
+        }
+        confirmLabel.do {
+            $0.text = "확인했어요"
+            // TODO: 머지하고 underline 넣기
+            $0.font = FontManager.body4Sb14.font
+            $0.textColor = .secondary300
+            $0.textAlignment = .center
+        }
+    }
+    
+    override func setUI() {
+        addSubviews(
+            backgroundImageView,
+            backgroundView,
+            titleLabel,
+            imageView,
+            descriptionLabel,
+            confirmLabel
+        )
+    }
+    
+    override func setLayout() {
+        backgroundImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(74.5.adjustedH)
+            $0.centerX.equalToSuperview()
+        }
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(24.adjustedH)
+            $0.centerX.equalToSuperview()
+        }
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(24.adjustedH)
+            $0.centerX.equalToSuperview()
+        }
+        confirmLabel.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(24.adjustedH)
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Home/View/JourneyResultViewController.swift
@@ -0,0 +1,48 @@
+//
+//  JourneyResultViewController.swift
+//  ByeBoo-iOS
+//
+//  Created by 최주리 on 7/9/25.
+//
+
+import UIKit
+
+final class JourneyResultViewController: BaseViewController {
+
+    private let rootView = JourneyResultView(
+        name: "하츠핑",
+        journeyType: .face,
+        journeyDescription:
+"""
+너무 힘든 시간을 보내고 있는 당신에게, 
+‘감정 직면 여정’을 추천해요.
+  이 여정은 감정을 직면하고, 
+상황을 정리하며, 점차 앞으로 나아가는
+5단계로 구성되어 있어요.
+  하루에 하나씩 기록해 나가다 보면, 
+감정이 조금씩 정돈되고
+마음이 가벼워질 거예요.
+"""
+    )
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func setAddTarget() {
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(confirmLabelTapped))
+        rootView.confirmLabel.addGestureRecognizer(tapRecognizer)
+        rootView.confirmLabel.isUserInteractionEnabled = true
+    }
+}
+
+extension JourneyResultViewController {
+    @objc
+    private func confirmLabelTapped() {
+        
+    }
+}
