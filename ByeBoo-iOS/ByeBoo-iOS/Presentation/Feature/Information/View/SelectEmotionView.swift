09adb91568cf6134ff9f9ae36c3ca99d9aa18075
heoseungjun <gjtmdwns0815@naver.com>
2025-07-08 11:08:02 +0900

    chore : 빌드 오류 해결을 위한 빈 파일 추가

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Information/View/SelectEmotionView.swift
@@ -0,0 +1,15 @@
+//
+//  SelectEmotionView.swift
+//  ByeBoo-iOS
+//
+//  Created by APPLE on 7/8/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SelectEmotionView: BaseView {
+    
+}
