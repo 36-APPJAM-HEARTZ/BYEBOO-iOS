491f6217d255202e6d865c524e306786d36af469
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-10 00:02:32 +0900

    feat: #70 퀘스트 팁 뷰컨트롤러

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Quest/View/QuestTipViewController.swift
@@ -0,0 +1,53 @@
+//
+//  QuestTipViewController.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/9/25.
+//
+
+import Combine
+import UIKit
+
+final class QuestTipViewController: BaseViewController {
+    
+    private let rootView = QuestTipView()
+    private let viewModel = QuestTipViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ByeBooNavigationBar.makeNavigationBar(
+            navigationItem: self.navigationItem,
+            navigationController: self.navigationController,
+            type: .titleAndClose("퀘스트 작성 TIP"),
+            action: #selector(close)
+        )
+        
+        bind()
+        viewModel.action(.tagButtonDidTap)
+    }
+    
+    private func bind() {
+        viewModel.output.questTipPublisher
+            .sink { [weak self] result in
+                switch result {
+                case .success(let entity):
+                    self?.rootView.bind(with: entity)
+                case .failure(let error):
+                    print("에러 발생: \(error)")
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension QuestTipViewController: Dismissible {
+    func close() {
+        dismiss(animated: true)
+    }
+}
