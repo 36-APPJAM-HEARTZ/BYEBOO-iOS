9437dbb83b2b1ede75eb722b1944a38525ebc7e8
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-10 00:01:00 +0900

    feat: #70 tip tag 프로토콜

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Protocol/TipTagDidTapProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  TipTagDidTapProtocol.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/9/25.
+//
+
+import Foundation
+
+@objc protocol TipTagDidTapProtocol {
+    @objc func tipTagDidTap()
+}
