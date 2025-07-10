e088653357fdec0482f1fdc3918126c64d139131
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-10 23:24:21 +0900

    feat: #83 mvvm 적용

---
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Entity/QuestAnswerEntity.swift
@@ -0,0 +1,19 @@
+//
+//  QuestAnswerEntity.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/10/25.
+//
+
+import Foundation
+
+struct QuestAnswerEntity {
+    var stepNumber: Int
+    var questNumber: Int
+    var createdAt: String
+    var question: String
+    var answer: String
+    var questEmotionState: String
+    var imageUrl: String?
+    var emotionDescription: String
+}
---
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Interface/QuestAnswerInterface.swift
@@ -0,0 +1,10 @@
+//
+//  QuestAnswerInterface.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/10/25.
+//
+
+protocol QuestAnswerInterface {
+    func fetchQuestAnswer() async throws -> QuestAnswerEntity
+}
