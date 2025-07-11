db944bc5c97898eec57486efd4b49325a63516b5
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-11 12:21:17 +0900

    feat: #83 퀘스트 행동형 VC

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/WriteQuest/ActivationType/View/CompleteActiveTypeQuestViewController.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/WriteQuest/ActivationType/View/CompleteActiveTypeQuestViewController.swift
@@ -5,17 +5,55 @@
 //  Created by 이나연 on 7/10/25.
 //
 
+import Combine
 import UIKit
 
 final class CompleteActiveTypeQuestViewController: BaseViewController {
-    private let rootView = CompleteActiveTypeQuestView()
     
+    private let rootView = CompleteActiveTypeQuestView()
+    private let viewModel = CompleteQuestViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
+
     override func loadView() {
         view = rootView
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationItem.hidesBackButton = true
+        viewModel.action(.questAnswerDidLoad)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        ByeBooLogger.debug("뷰 디드로드")
+        
+        ByeBooNavigationBar.makeNavigationBar(
+            navigationItem: self.navigationItem,
+            navigationController: self.navigationController,
+            type: .close,
+            action: #selector(close)
+        )
+        
+        bind()
+    }
+    
+    private func bind() {
+        viewModel.output.resultPublisher
+            .sink { [weak self] result in
+                switch result {
+                case .success(let entity):
+                    self?.rootView.bind(with: entity)
+                case .failure(let error):
+                    ByeBooLogger.debug(error)
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension CompleteActiveTypeQuestViewController: Dismissible {
+    @objc func close() {
+        // TODO: - 퀘스트 메인 이동 수정
+        self.navigationController?.popViewController(animated: true)
     }
 }
