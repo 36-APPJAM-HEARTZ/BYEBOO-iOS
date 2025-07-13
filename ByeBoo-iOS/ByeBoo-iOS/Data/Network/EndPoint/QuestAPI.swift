670dec7d96b91cb8a4f96dad1e29720981e802f2
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-13 21:23:38 +0900

    feat: #88 QuestAPI

---
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Network/EndPoint/QuestAPI.swift
@@ -0,0 +1,94 @@
+//
+//  QuestAPI.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/13/25.
+//
+
+import Foundation
+
+import Alamofire
+
+enum QuestAPI {
+    case checkQuest(userID: Int, questID: Int)
+    case recording(userID: Int, questID: Int)
+    case active(userID: Int, questID: Int)
+    case images(userID: Int)
+    case tip(userID: Int, questID: Int)
+}
+
+extension QuestAPI: EndPoint {
+    var basePath: String {
+        return "/api/v1/quests"
+    }
+    
+    var path: String {
+        switch self {
+        case .checkQuest(_, let questID):
+            return "/\(questID)"
+        case .recording(_, let questID):
+            return "/\(questID)/recording"
+        case .active(_, let questID):
+            return "/\(questID)/active"
+        case .images:
+            return "/images/signed-url"
+        case .tip(_, let questID):
+            return "/\(questID)/tip"
+        }
+    }
+    
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .checkQuest:
+            return .get
+        case .recording:
+            return .post
+        case .active:
+            return .post
+        case .images:
+            return .post
+        case .tip:
+            return .get
+        }
+    }
+    
+    var headers: HeaderType {
+        switch self {
+        case let .checkQuest(userID, _),
+            let .recording(userID, _),
+            let .active(userID, _),
+            let .tip(userID, _),
+            let .images(userID):
+            return .withAuth(userID: userID)
+        }
+    }
+    
+    var parameterEncoding: any Alamofire.ParameterEncoding {
+        switch self {
+        case .checkQuest, .tip:
+            return URLEncoding.default
+        case .recording, .active, .images:
+            return JSONEncoding.default
+        }
+    }
+    
+    var queryParameters: [String : String]? {
+        switch self {
+        case let .checkQuest(_, questID):
+            return ["questID": "\(questID)"]
+        case let .recording(_, questID):
+            return ["questID": "\(questID)"]
+        case let .active(_, questID):
+            return ["questID": "\(questID)"]
+        case let .tip(_, questID):
+            return ["questID": "\(questID)"]
+        case .images:
+            return nil
+        }
+    }
+    
+    var bodyParameters: Alamofire.Parameters? {
+        nil
+    }
+    
+}
