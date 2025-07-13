06ac7bcb05d76aa29720b5ff8ce470ab1c734524
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-13 21:23:25 +0900

    feat: #88 DTO

---
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Model/SaveQuestRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  SaveQuestDTO.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/13/25.
+//
+
+import Foundation
+
+struct SaveQuestDTO: Encodable {
+    var questId: Int
+    var isComplete: Bool
+}
