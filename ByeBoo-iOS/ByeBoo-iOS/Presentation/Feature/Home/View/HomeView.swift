5196c4dc34cdd4f9bf9bae0baf5acd8271dbe4be
JURI <80569323+juri123123@users.noreply.github.com>
2025-07-13 13:33:43 +0900

    add: #90 로띠 홈 에셋 수정

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Home/View/HomeView.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Home/View/HomeView.swift
@@ -11,7 +11,7 @@ import Lottie
 
 final class HomeView: BaseView {
 
-    private let backgroundImageView = LottieAnimationView(name: "Bori_home_4")
+    private let backgroundImageView = LottieAnimationView(name: "Bori_home8")
     private let headerView = HomeHeaderView(state: .beforeQuest)
     
     override func setStyle() {
