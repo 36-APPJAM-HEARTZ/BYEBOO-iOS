d363c6a1c1a31aa2234546b4a0f7ae9a4a856730
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-13 21:43:53 +0900

    feat: #88 퀘스트 저장 레포지토리

---
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Interface/SaveQuestTypeInterface.swift
@@ -0,0 +1,12 @@
+//
+//  SaveQuestTypeInterface.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/13/25.
+//
+
+import Foundation
+
+protocol SaveQuestTypeInterface {
+    func postSaveQuest(questID: Int) async throws -> Void
+}
