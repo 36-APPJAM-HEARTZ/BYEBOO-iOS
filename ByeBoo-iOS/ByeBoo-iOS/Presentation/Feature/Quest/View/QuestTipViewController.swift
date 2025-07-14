0aaf13788eade4880875d4bfd3113226d94bf6b4
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-14 22:38:37 +0900

    refactor: #106 코드리뷰 반영

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Quest/View/QuestTipViewController.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Quest/View/QuestTipViewController.swift
@@ -26,14 +26,12 @@ final class QuestTipViewController: BaseViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.navigationItem.hidesBackButton = true
-        viewModel.action(.questTipDidLoad(questID: 1))
-    }
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = true
+        viewModel.action(.questTipDidLoad(questID: 1))
         
         ByeBooNavigationBar.makeNavigationBar(
             navigationItem: self.navigationItem,
