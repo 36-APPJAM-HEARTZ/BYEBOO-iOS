2fb86b2ce3ade798d950721cb3cb50fd6403286f
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-15 03:07:41 +0900

    feat: #98 dto

---
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Model/SignedURLRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  SignedURLRequestDTO.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 7/14/25.
+//
+
+import Foundation
+
+struct SignedURLRequestDTO: Codable {
+    var contentType: String
+    var imageKey: String
+}
