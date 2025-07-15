b35ba5f26e157e0cb9ca1df26ee5dfd699e27ecf
yeonee <leenayeon@sookmyung.ac.kr>
2025-07-15 22:47:07 +0900

    refactor: #120 키보드 한번만 올라오게 수정

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/WriteQuest/ActivationType/View/WriteActiveTypeQuestViewController.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/WriteQuest/ActivationType/View/WriteActiveTypeQuestViewController.swift
@@ -18,6 +18,7 @@ final class WriteActiveTypeQuestViewController: BaseViewController {
     private var answerText: String = ""
     private var emotionState: String = ""
     private var image: UIImage = UIImage()
+    private var isKeyboardUsed: Bool = false
     
     override func loadView() {
         view = rootView
@@ -81,16 +82,20 @@ final class WriteActiveTypeQuestViewController: BaseViewController {
 extension WriteActiveTypeQuestViewController {
     @objc
     private func textViewMoveUp(_ notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            UIView.animate(withDuration: 0.3, animations: {
-                let offsetY = keyboardSize.height
-                self.rootView.transform = CGAffineTransform(translationX: 0, y: -offsetY)
-            })
+        if !self.isKeyboardUsed {
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                UIView.animate(withDuration: 0.3, animations: {
+                    let offsetY = keyboardSize.height
+                    self.rootView.transform = CGAffineTransform(translationX: 0, y: -offsetY)
+                })
+                self.isKeyboardUsed = true
+            }
         }
     }
     
     @objc
     private func textViewMoveDown() {
+        self.isKeyboardUsed = false
         self.view.transform = .identity
     }
     
--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/WriteQuest/QuestionType/View/WriteQuestionTypeQuestViewController.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/WriteQuest/QuestionType/View/WriteQuestionTypeQuestViewController.swift
@@ -17,6 +17,7 @@ final class WriteQuestionTypeQuestViewController: BaseViewController {
     let questID: Int = 0
     private var answerText: String = ""
     private var emotionState: String = ""
+    private var isKeyboardUsed: Bool = false
     
     init(
         viewModel: WriteQuestionTypeViewModel
@@ -65,16 +66,20 @@ final class WriteQuestionTypeQuestViewController: BaseViewController {
 extension WriteQuestionTypeQuestViewController {
     @objc
     private func textViewMoveUp(_ notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            UIView.animate(withDuration: 0.3, animations: {
-                let offsetY = keyboardSize.height - self.rootView.safeAreaInsets.bottom * 4
-                self.rootView.transform = CGAffineTransform(translationX: 0, y: -offsetY)
-            })
+        if !self.isKeyboardUsed{
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                UIView.animate(withDuration: 0.3, animations: {
+                    let offsetY = keyboardSize.height - self.rootView.safeAreaInsets.bottom * 4
+                    self.rootView.transform = CGAffineTransform(translationX: 0, y: -offsetY)
+                    self.isKeyboardUsed = true
+                })
+            }
         }
     }
     
     @objc
     private func textViewMoveDown() {
+        isKeyboardUsed = false
         self.rootView.transform = .identity
     }
     
