9e8fcdcdbfef0610d236c4e4583c926b0f6f8ad9
heoseungjun <gjtmdwns0815@naver.com>
2025-08-16 20:04:03 +0900

    feat: #172 이미지 리사이즈 함수 확장 구현

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Extension/UIImage+.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage+.swift
+//  ByeBoo-iOS
+//
+//  Created by APPLE on 8/14/25.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func resize(newWidth: CGFloat) -> UIImage {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
+
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { context in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        return renderImage
+    }
+}
