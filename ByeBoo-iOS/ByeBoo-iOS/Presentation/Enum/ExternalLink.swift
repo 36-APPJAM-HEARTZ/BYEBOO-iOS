e91ef37e81b8fa16d9e72eb79a2942dcc8cf578e
heoseungjun <gjtmdwns0815@naver.com>
2025-08-19 14:46:58 +0900

    feat: #185 외부 링크 관리

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Enum/ExternalLink.swift
@@ -0,0 +1,12 @@
+//
+//  ExternalLink.swift
+//  ByeBoo-iOS
+//
+//  Created by APPLE on 8/19/25.
+//
+
+enum ExternalLink: String {
+    
+    case termsOfService = "https://lively-mars-3b7.notion.site/24cab823e68d801aac95ec5d0389d192"
+    case privacyPolicy = "https://lively-mars-3b7.notion.site/24cab823e68d80a19ab1fbf87d6cfbc3"
+}
