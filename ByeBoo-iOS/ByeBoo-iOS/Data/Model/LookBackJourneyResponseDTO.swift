83961257efcf3b3d3d556d6d6dfe888601fe6298
yeonee <leenayeon@sookmyung.ac.kr>
2025-08-21 03:10:41 +0900

    feat: #189 DTO 추가

---
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Model/LookBackJourneyResponseDTO.swift
@@ -0,0 +1,38 @@
+//
+//  LookBackJourneyResponseDTO.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 8/20/25.
+//
+
+struct LookBackJourneyResponseDTO: Decodable{
+    let inCompletedCount: Int
+    let inCompletedJourneys: [JourneyStyleData]
+    let completedCount: Int
+    let completedJourneys: [JourneyStyleData]
+}
+
+struct JourneyStyleData: Decodable {
+    let journey: String
+    let style: String
+}
+
+extension LookBackJourneyResponseDTO {
+    func toEntity() -> LookBackJourneyEntity {
+        .init(
+            inCompletedCount: inCompletedCount,
+            inCompletedJourneys: inCompletedJourneys.map { $0.toEntity() },
+            completedCount: completedCount,
+            completedJourneys: completedJourneys.map { $0.toEntity() }
+        )
+    }
+}
+
+extension JourneyStyleData {
+    func toEntity() -> JourneyStyleEntity {
+        .init(
+            journey: journey,
+            style: style
+        )
+    }
+}
