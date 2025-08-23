444760150f41784ad549bfb507c311237f2544ec
JURI <80569323+juri123123@users.noreply.github.com>
2025-08-23 15:07:32 +0900

    feat: #183 헬퍼 뜨는 로직 적용

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Home/ViewModel/HomeViewModel.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Home/ViewModel/HomeViewModel.swift
@@ -14,14 +14,13 @@ final class HomeViewModel {
     
     private(set) var output: Output
     private var characterResultSubject = PassthroughSubject<Result<String, ByeBooError>, Never>()
-    private var countResultSubject = PassthroughSubject<Result<Int, ByeBooError>, Never>()
     private var userResultSubject = CurrentValueSubject<String, Never>("하츠핑")
-    private var isHelperShownResultSubject = CurrentValueSubject<Bool, Never>(true)
-    private var homeStateResultSubject = PassthroughSubject<Result<HomeState, ByeBooError>, Never>()
+    private var isHelperShownResultSubject = CurrentValueSubject<Bool, Never>(false)
+    private var homeStateResultSubject = PassthroughSubject<Result<UserQuestStatusEntity, ByeBooError>, Never>()
     private var journeyResultSubject = PassthroughSubject<Result<JourneyEntity, ByeBooError>, Never>()
 
     private let fetchCharacterDialogueUseCase: FetchCharacterDialogueUseCase
-    private let fetchCompleteQuestCountUseCase: FetchCompleteQuestCountUseCase
+    private let fetchQuestStatusUseCase: FetchQuestStatusUseCase
     private let fetchUserJourneyUseCase: FetchUserJourneyUseCase
     private let getUserNameUseCase: GetUserNameUseCase
     private let setHelperUseCase: SetHelperUseCase
@@ -29,14 +28,14 @@ final class HomeViewModel {
     
     init(
         fetchCharacterDialogueUseCase: FetchCharacterDialogueUseCase,
-        fetchCompleteQuestCountUseCase: FetchCompleteQuestCountUseCase,
+        fetchQuestStatusUseCase: FetchQuestStatusUseCase,
         fetchUserJourneyUseCase: FetchUserJourneyUseCase,
         getUserNameUseCase: GetUserNameUseCase,
         setHelperUseCase: SetHelperUseCase,
         getHelperUseCase: GetHelperUseCase
     ) {
         self.fetchCharacterDialogueUseCase = fetchCharacterDialogueUseCase
-        self.fetchCompleteQuestCountUseCase = fetchCompleteQuestCountUseCase
+        self.fetchQuestStatusUseCase = fetchQuestStatusUseCase
         self.fetchUserJourneyUseCase = fetchUserJourneyUseCase
         self.getUserNameUseCase = getUserNameUseCase
         self.setHelperUseCase = setHelperUseCase
@@ -44,7 +43,6 @@ final class HomeViewModel {
         
         output = Output(
             characterResult: characterResultSubject.eraseToAnyPublisher(),
-            countResult: countResultSubject.eraseToAnyPublisher(),
             userResult: userResultSubject.eraseToAnyPublisher(),
             helperResult: isHelperShownResultSubject.eraseToAnyPublisher(),
             homeStateResult: homeStateResultSubject.eraseToAnyPublisher(),
@@ -61,10 +59,9 @@ extension HomeViewModel: ViewModelType {
     
     struct Output {
         let characterResult: AnyPublisher<Result<String, ByeBooError>, Never>
-        let countResult: AnyPublisher<Result<Int, ByeBooError>, Never>
         let userResult: AnyPublisher<String, Never>
         let helperResult: AnyPublisher<Bool, Never>
-        let homeStateResult: AnyPublisher<Result<HomeState, ByeBooError>, Never>
+        let homeStateResult: AnyPublisher<Result<UserQuestStatusEntity, ByeBooError>, Never>
         let journeyResult: AnyPublisher<Result<JourneyEntity, ByeBooError>, Never>
     }
     
@@ -73,8 +70,8 @@ extension HomeViewModel: ViewModelType {
         case .viewWillAppear:
             // TODO: 구조적 동시성 반영
             fetchDialogue()
-            fetchCount()
-            getUserResult()
+            fetchStatus()
+            fetchJourney()
         case .helperTapped:
             setHelperShown()
         }
@@ -97,21 +94,17 @@ extension HomeViewModel {
         }
     }
     
-    private func fetchCount() {
+    private func fetchStatus() {
         Task {
             do {
-                let count = try await fetchCompleteQuestCountUseCase.execute()
-                homeStateResultSubject.send(.success(.beforeQuest))
-                countResultSubject.send(.success(count))
+                let status = try await fetchQuestStatusUseCase.execute()
+                homeStateResultSubject.send(.success(status))
+                isHelperShown(state: status.currentStatus)
             } catch {
                 if let error = error as? ByeBooError {
-                    switch error {
-                    case .notFoundQuest:
-                        fetchJourney()
-                    default:
-                        countResultSubject.send(.failure(error))
-                    }
+                    homeStateResultSubject.send(.failure(error))
                 }
+                isHelperShown(state: .beforeJourneyStart)
             }
         }
     }
@@ -120,14 +113,8 @@ extension HomeViewModel {
         Task {
             do {
                 let journey = try await fetchUserJourneyUseCase.execute()
-                homeStateResultSubject.send(.success(.beforeJourneyStart))
                 journeyResultSubject.send(.success(journey))
             } catch {
-                homeStateResultSubject.send(
-                    .failure(
-                        error as? ByeBooError ?? ByeBooError.unknownError
-                    )
-                )
                 journeyResultSubject.send(
                     .failure(
                         error as? ByeBooError ?? ByeBooError.unknownError
@@ -139,10 +126,15 @@ extension HomeViewModel {
     
     private func getUserResult() {
         let name = getUserNameUseCase.execute()
-        let isHelperShown = getHelperUseCase.execute()
-        ByeBooLogger.debug("isHelperShown: \(isHelperShown)")
         userResultSubject.send(name)
-        isHelperShownResultSubject.send(isHelperShown)
+    }
+    
+    private func isHelperShown(state: HomeState) {
+        if getHelperUseCase.execute() && state == .beforeJourneyStart {
+            isHelperShownResultSubject.send(true)
+        } else {
+            isHelperShownResultSubject.send(false)
+        }
     }
     
     private func setHelperShown() {
