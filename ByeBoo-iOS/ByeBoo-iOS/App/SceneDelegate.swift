91d4572daa8a8c114aec729bbfa25e88877511c9
yeonee <leenayeon@sookmyung.ac.kr>
2025-08-24 17:14:05 +0900

    chore: #189 로그인 연결 전 scenedelegate 변경

--- ByeBoo-iOS/ByeBoo-iOS/App/SceneDelegate.swift
+++ ByeBoo-iOS/ByeBoo-iOS/App/SceneDelegate.swift
@@ -19,18 +19,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         DIContainer.shared.dependencyInject()
-        
-        guard let viewModel = DIContainer.shared.resolve(type: InformationViewModel.self) else {
-            ByeBooLogger.error(ByeBooError.DIFailedError)
-            fatalError()
-        }
-        
-        let viewController = InformationViewController(viewModel: viewModel)
 
-        let navigationController = UINavigationController(rootViewController: viewController)
+        let viewController = BottomNavigationViewController()
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = navigationController
+        window.rootViewController = viewController
         window.makeKeyAndVisible()
         
         self.window = window
