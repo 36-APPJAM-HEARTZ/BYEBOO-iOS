d79f06290ccd4e932cca7987019870f91d1182f9
yeonee <leenayeon@sookmyung.ac.kr>
2025-08-25 00:50:44 +0900

    feat: #203 서버 전달용 data layer extension

---
+++ ByeBoo-iOS/ByeBoo-iOS/Data/Enum/JourneyType+Data.swift
@@ -0,0 +1,19 @@
+//
+//  JourneyType+Data.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 8/24/25.
+//
+
+extension JourneyType {
+    static func toEnum(_ rawValue: String) -> String {
+        switch rawValue {
+        case "감정 직면":
+            return "FACE_EMOTION"
+        case "감정 정리":
+            return "PROCESS_EMOTION"
+        default:
+            return ""
+        }
+    }
+}
