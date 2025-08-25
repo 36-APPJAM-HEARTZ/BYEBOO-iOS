10582f2c0d5bb2ad0beebc930e2754174030ee2c
JURI <80569323+juri123123@users.noreply.github.com>
2025-08-25 22:50:30 +0900

    add: #206 타겟 추가

---
+++ ByeBoo-iOS/ByeBooTests/ByeBooTests.swift
@@ -0,0 +1,16 @@
+//
+//  ByeBooTests.swift
+//  ByeBooTests
+//
+//  Created by 최주리 on 8/25/25.
+//
+
+import Testing
+
+struct ByeBooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
