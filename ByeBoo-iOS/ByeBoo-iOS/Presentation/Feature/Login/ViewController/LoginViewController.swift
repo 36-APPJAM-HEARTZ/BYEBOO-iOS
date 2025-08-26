6e93af33d576d890c95c2701981d8305dacde076
yeonee <leenayeon@sookmyung.ac.kr>
2025-08-27 02:30:05 +0900

    feat: #207 Presentation Layer 작성

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Login/ViewController/LoginViewController.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Login/ViewController/LoginViewController.swift
@@ -5,13 +5,68 @@
 //  Created by 이나연 on 8/19/25.
 //
 
+import Combine
 import UIKit
 
 final class LoginViewController: BaseViewController {
     
     private let rootView = LoginView()
+    private let viewModel: LoginViewModel
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         view = rootView
+        setAddTarget()
+        bind()
     }
+    
+    override func setAddTarget() {
+        rootView.kakaoLoginButton.addTarget(self, action: #selector(kakaoLoginButtonDidTap) , for: .touchUpInside)
+        rootView.appleLoginButton.addTarget(self, action: #selector(appleLoginButtonDidTap), for: .touchUpInside)
+    }
+}
+
+extension LoginViewController {
+    @objc
+    func kakaoLoginButtonDidTap() {
+        ByeBooLogger.debug("로그인 버튼 터치됨")
+        viewModel.action(.kakaoLoginButtonDidTap)
+    }
+    
+    @objc
+    func appleLoginButtonDidTap() {
+        
+    }
+}
+
+extension LoginViewController {
+    private func bind() {
+        viewModel.output.isRegisteredPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] result in
+                switch result {
+                case .success(let isRegisterd):
+                    let nextViewController: UIViewController
+                    if isRegisterd {
+                        nextViewController = BottomNavigationViewController()
+                    } else {
+                        nextViewController = ViewControllerFactory.shared.makeInformationViewController()
+                    }
+                    self?.navigationController?.setViewControllers([nextViewController], animated: false)
+                case .failure(let error):
+                    ByeBooLogger.debug(error)
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
 }
---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Feature/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,102 @@
+//
+//  LoginViewModel.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 8/26/25.
+//
+
+import Combine
+import Foundation
+
+final class LoginViewModel {
+    
+    private var kakaoLoginAuthSubject: PassthroughSubject<Result<Void, ByeBooError>, Never> = .init()
+    private var postLoginSubject: PassthroughSubject<Result<Void, ByeBooError>, Never> = .init()
+    private var isRegisteredSubject: PassthroughSubject<Result<Bool, ByeBooError>, Never> = .init()
+    
+    var output: Output {
+        Output(
+            isRegisteredPublisher: isRegisteredSubject.eraseToAnyPublisher()
+        )
+    }
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    private let kakaoLoginUseCase: KakaoLoginUseCase
+    private let postLoginUseCase: PostLoginUseCase
+    private let getIsRegisteredUseCase: GetIsRegisteredUseCase
+    
+    init(
+        kakaoLoginUseCase: KakaoLoginUseCase,
+        postLoginUseCase: PostLoginUseCase,
+        getIsRegisteredUseCase: GetIsRegisteredUseCase
+    ) {
+        self.kakaoLoginUseCase = kakaoLoginUseCase
+        self.postLoginUseCase = postLoginUseCase
+        self.getIsRegisteredUseCase = getIsRegisteredUseCase
+    }
+    
+}
+
+extension LoginViewModel {
+    enum Input {
+        case kakaoLoginButtonDidTap
+        case appleLoginButtonDidTap
+    }
+    
+    struct Output {
+        let isRegisteredPublisher: AnyPublisher<Result<Bool, ByeBooError>, Never>
+    }
+    
+    func action(_ trigger: Input) {
+        switch trigger {
+        case .kakaoLoginButtonDidTap:
+            kakaoLogin()
+        case .appleLoginButtonDidTap:
+            appleLogin()
+        }
+    }
+}
+
+extension LoginViewModel {
+    private func kakaoLogin() {
+        Task {
+            do {
+                let _ = try await kakaoLoginUseCase.execute()
+                kakaoLoginAuthSubject.send(.success(()))
+                postLogin(platform: "KAKAO")
+            } catch {
+                guard let error = error as? ByeBooError else {
+                    return
+                }
+                ByeBooLogger.error(error as ByeBooError)
+                kakaoLoginAuthSubject.send(.failure(error))
+            }
+        }
+    }
+    
+    private func appleLogin() {
+        
+    }
+    
+    private func postLogin(platform: String) {
+        Task {
+            do {
+                let _ = try await postLoginUseCase.execute(platform: platform)
+                postLoginSubject.send(.success(()))
+                getIsRegistered()
+            } catch {
+                guard let error = error as? ByeBooError else {
+                    return
+                }
+                ByeBooLogger.error(error as ByeBooError)
+                postLoginSubject.send(.failure(error))
+            }
+        }
+    }
+    
+    private func getIsRegistered() {
+        let isRegistered = getIsRegisteredUseCase.execute()
+        isRegisteredSubject.send(.success(isRegistered))
+    }
+}
