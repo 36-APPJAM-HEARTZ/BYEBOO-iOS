8b75e6e07dca693b826837a254429d9022e37512
heoseungjun <gjtmdwns0815@naver.com>
2025-08-28 13:04:52 +0900

    feat: #211 시간 관련 enum 구현

---
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Entity/Enum/TimeConstant.swift
@@ -0,0 +1,11 @@
+//
+//  TimeConstant.swift
+//  ByeBoo-iOS
+//
+//  Created by APPLE on 8/28/25.
+//
+
+enum TimeConstant {
+    static let secondPerHour = 3600
+    static let secondPerMinute = 60
+}
