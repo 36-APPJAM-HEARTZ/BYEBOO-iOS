8f2090b153b83776a6fefab2203253cebcb4fe01
heoseungjun <gjtmdwns0815@naver.com>
2025-10-23 16:30:28 +0900

    refactor: #296 NicknameRule 생성

--- ByeBoo-iOS/ByeBoo-iOS/Domain/UseCase/CheckValidNicknameUseCase.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/UseCase/CheckValidNicknameUseCase.swift
@@ -5,15 +5,8 @@
 //  Created by APPLE on 10/21/25.
 //
 
-import Foundation
-
 protocol CheckValidNicknameUseCase {
     func execute(nickname: String) -> Bool
-}
-
-enum NicknameRule {
-    private static let regularExpression = "(?=.{2,5}$)(?!.*[ㄱ-ㅎㅏ-ㅣ])[가-힣a-zA-Z0-9]+"
-    static let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
 }
 
 struct DefaultCheckValidNicknameUseCase: CheckValidNicknameUseCase {
