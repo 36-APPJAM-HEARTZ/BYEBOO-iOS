4644739119dcf89876eb0fe4192c6664c22133e4
yeonee <leenayeon@sookmyung.ac.kr>
2025-11-12 00:16:58 +0900

    feat: #302 퀘스트 수정 관련 enum, protocol

---
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Protocol/EditQuestProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  EditQuestProtocol.swift
+//  ByeBoo-iOS
+//
+//  Created by 이나연 on 11/11/25.
+//
+
+protocol EditQuestProtocol {
+    func getExistingQuest(quest: String?, image: String?)
+}
