15257d1868dd1944f69ac54631772bea790f184d
yeonee <leenayeon@sookmyung.ac.kr>
2025-11-30 01:06:32 +0900

    style: #325 아이템과 타이틀 간 간격 조정

--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Common/Navigation/ByeBooTabBar.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Common/Navigation/ByeBooTabBar.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
-final class BottomNavigationViewController: UITabBarController {
+final class ByeBooTabBar: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .grayscale900
         
+        setValue(ByeBooTabBarView(), forKey: "tabBar")
         setViewController()
         setViewControllerAppearance()
     }
+    
     
     private func setViewController() {
         
@@ -50,19 +52,20 @@ final class BottomNavigationViewController: UITabBarController {
 
         let itemAppearance = UITabBarItemAppearance()
         itemAppearance.normal.iconColor = .grayscale400
+        itemAppearance.normal.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -4.adjustedH)
         itemAppearance.normal.titleTextAttributes = [
             .foregroundColor: UIColor.grayscale400,
             .font: FontManager.body6R14.font
         ]
         
         itemAppearance.selected.iconColor = .primary300
+        itemAppearance.selected.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -4.adjustedH)
         itemAppearance.selected.titleTextAttributes = [
             .foregroundColor: UIColor.primary300,
             .font: FontManager.body6R14.font
         ]
         
         appearance.stackedLayoutAppearance = itemAppearance
-        
         tabBar.standardAppearance = appearance
         tabBar.scrollEdgeAppearance = appearance
     }
