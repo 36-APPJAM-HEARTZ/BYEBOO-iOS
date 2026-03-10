6c5dd0ea33733974f864638ce3c80b6d0fd9470a
heoseungjun <gjtmdwns0815@naver.com>
2026-03-10 21:27:56 +0900

    feat: #393 공통퀘스트 삭제 API 연결

--- ByeBoo-iOS/ByeBoo-iOS/Domain/Interface/CommonQuestInterface.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Interface/CommonQuestInterface.swift
@@ -11,4 +11,5 @@ protocol CommonQuestInterface {
     func saveCommonQuest(questID: Int, answer: String) async throws
     func fetchCommonQuest(date: String, cursor: Int?) async throws -> CommonQuestAnswersEntity
     func updateCommonQuest(answerID: Int, answer: String) async throws
+    func deleteCommonQuest(answerID: Int) async throws
 }
--- ByeBoo-iOS/ByeBoo-iOS/Presentation/Common/BottomSheet/CommonQuest/CommonQuestBottomSheetViewModel.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Presentation/Common/BottomSheet/CommonQuest/CommonQuestBottomSheetViewModel.swift
@@ -12,24 +12,29 @@ final class CommonQuestBottomSheetViewModel {
     
     private var reportQuestSubject: PassthroughSubject<Result<Void, ByeBooError>, Never> = .init()
     private var blockUserSubject: PassthroughSubject<Result<Void, ByeBooError>, Never> = .init()
+    private var deleteQuestSubject: PassthroughSubject<Result<Void, ByeBooError>, Never> = .init()
     
     var output: Output {
         Output(
             reportQuestPublisher: reportQuestSubject.eraseToAnyPublisher(),
-            blockUserPublisher: blockUserSubject.eraseToAnyPublisher()
+            blockUserPublisher: blockUserSubject.eraseToAnyPublisher(),
+            deleteQuestPublisher: deleteQuestSubject.eraseToAnyPublisher()
         )
     }
     
     private let blockUserUseCase: BlockUserUseCase
     private let reportCommonQuestUseCase: ReportsCommonQuestAnswerUseCase
+    private let deleteCommonQuestUseCase: DeleteCommonQuestUseCase
     private var cancellables = Set<AnyCancellable>()
     
     init(
         blockUserUseCase: BlockUserUseCase,
-        reportCommonQuestUseCase: ReportsCommonQuestAnswerUseCase
+        reportCommonQuestUseCase: ReportsCommonQuestAnswerUseCase,
+        deleteCommonQuestUseCase: DeleteCommonQuestUseCase
     ) {
         self.blockUserUseCase = blockUserUseCase
         self.reportCommonQuestUseCase = reportCommonQuestUseCase
+        self.deleteCommonQuestUseCase = deleteCommonQuestUseCase
     }
 }
 
@@ -37,11 +42,13 @@ extension CommonQuestBottomSheetViewModel: ViewModelType {
     enum Input {
         case block(userID: Int)
         case report(answerID: Int)
+        case delete(answerID: Int)
     }
     
     struct Output {
         let reportQuestPublisher: AnyPublisher<Result<Void, ByeBooError>, Never>
         let blockUserPublisher: AnyPublisher<Result<Void, ByeBooError>, Never>
+        let deleteQuestPublisher: AnyPublisher<Result<Void, ByeBooError>, Never>
     }
     
     func action(_ trigger: Input) {
@@ -50,6 +57,8 @@ extension CommonQuestBottomSheetViewModel: ViewModelType {
             blockUser(userID: userID)
         case .report(let answerID):
             reportCommonQuest(answerID: answerID)
+        case .delete(let answerID):
+            deleteCommonQuest(answerID: answerID)
         }
     }
 }
@@ -61,11 +70,7 @@ extension CommonQuestBottomSheetViewModel {
                 try await blockUserUseCase.execute(userID: userID)
                 blockUserSubject.send(.success(()))
             } catch {
-                guard let error = error as? ByeBooError else {
-                    return
-                }
-                ByeBooLogger.error(error as ByeBooError)
-                blockUserSubject.send(.failure(error))
+                sendError(error: error)
             }
         }
     }
@@ -76,12 +81,27 @@ extension CommonQuestBottomSheetViewModel {
                 try await reportCommonQuestUseCase.execute(answerID: answerID)
                 reportQuestSubject.send(.success(()))
             } catch {
-                guard let error = error as? ByeBooError else {
-                    return
-                }
-                ByeBooLogger.error(error as ByeBooError)
-                reportQuestSubject.send(.failure(error))
+                sendError(error: error)
             }
         }
+    }
+    
+    private func deleteCommonQuest(answerID: Int) {
+        Task {
+            do {
+                try await deleteCommonQuestUseCase.execute(answerID: answerID)
+                deleteQuestSubject.send(.success(()))
+            } catch {
+                sendError(error: error)
+            }
+        }
+    }
+    
+    private func sendError(error: Error) {
+        guard let error = error as? ByeBooError else {
+            return
+        }
+        ByeBooLogger.error(error as ByeBooError)
+        reportQuestSubject.send(.failure(error))
     }
 }
