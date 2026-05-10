2da80d8ad8b866e9ca1d1e5cea2bdcc1f44971d0
yeonee <leenayeon@sookmyung.ac.kr>
2026-05-10 21:53:58 +0900

    chore: #428 comment entity stub 추가

--- ByeBoo-iOS/ByeBoo-iOS/Domain/Entity/CommonQuestCommentEntity.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Entity/CommonQuestCommentEntity.swift
@@ -22,7 +22,7 @@ extension CommonQuestCommentEntity {
         return (1...5).map {
             .init(
                 commentID: $0,
-                replyCount: 3,
+                replyCount: $0 - 1,
                 writerID: 2,
                 writer: "장원영",
                 profileIcon: "SADNESS",
@@ -31,6 +31,33 @@ extension CommonQuestCommentEntity {
             )
         }
     }
+    
+    static func toCommentStub() -> Self {
+        .init(
+            commentID: 1,
+            replyCount: 2,
+            writerID: 2,
+            writer: "가을이",
+            profileIcon: "SELF_UNDERSTANDING",
+            writtenAt: "2026-02-19T02:09:43.735730",
+            content: "나도여ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ냐냐냐냐냐냐냐냐냐"
+        )
+    }
+    
+    static func toReplyListStub() -> [Self] {
+        return (1...5).map {
+            .init(
+                commentID: $0,
+                replyCount: nil,
+                writerID: 2,
+                writer: "안유진",
+                profileIcon: "SADNESS",
+                writtenAt: "2026-02-19T02:09:43.735730",
+                content: "헤어짐ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ"
+            )
+        }
+    }
+    
     
     static func toReplyStub() -> Self {
         .init(
