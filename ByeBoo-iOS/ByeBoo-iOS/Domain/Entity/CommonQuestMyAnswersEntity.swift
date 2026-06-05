9669d2e8b4a96ee1d1d0584bde4bfa72b9a2c1f4
yeonee <leenayeon@sookmyung.ac.kr>
2026-06-06 00:28:59 +0900

    refactor: #444 엔티티 수정

--- ByeBoo-iOS/ByeBoo-iOS/Domain/Entity/CommonQuestMyAnswersEntity.swift
+++ ByeBoo-iOS/ByeBoo-iOS/Domain/Entity/CommonQuestMyAnswersEntity.swift
@@ -18,6 +18,10 @@ struct CommonQuestMyAnswerEntity {
     let writtenAt: String
     let content: String
     let question: String
+    let nickname: String
+    let isLiked: Bool
+    let likeCount: Int
+    let commentCount: Int
 }
 
 extension CommonQuestMyAnswersEntity {
@@ -27,7 +31,11 @@ extension CommonQuestMyAnswersEntity {
             answerID: $0,
             writtenAt: Date.now.toString(),
             content: "\($0)번째 테스트 답변",
-            question: "\($0)번째 질문"
+            question: "\($0)번째 질문",
+            nickname: "냔",
+            isLiked: false,
+            likeCount: 2,
+            commentCount: 3
         )
     }
     
@@ -47,10 +55,22 @@ extension CommonQuestMyAnswerEntity {
     
     static func stubs() -> [Self] {
         [
-            .init(answerID: 1, writtenAt: "2025-01-11", content: "content", question: "question"),
-            .init(answerID: 2, writtenAt: "2025-01-12", content: "content", question: "question"),
-            .init(answerID: 3, writtenAt: "2025-01-13", content: "content", question: "question"),
-            .init(answerID: 4, writtenAt: "2025-01-14", content: "content", question: "question")
+            .init(
+                answerID: 1, writtenAt: "2025-01-11", content: "content",
+                question: "question", nickname: "가을", isLiked: false, likeCount: 1, commentCount: 2
+            ),
+            .init(
+                answerID: 2, writtenAt: "2025-01-12", content: "content",
+                question: "question", nickname: "원영", isLiked: false, likeCount: 1, commentCount: 2
+            ),
+            .init(
+                answerID: 3, writtenAt: "2025-01-13", content: "content",
+                question: "question", nickname: "리즈", isLiked: false, likeCount: 1, commentCount: 2
+            ),
+            .init(
+                answerID: 4, writtenAt: "2025-01-14", content: "content",
+                question: "question", nickname: "유진", isLiked: false, likeCount: 1, commentCount: 2
+            )
         ]
     }
 }
